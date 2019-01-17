f7550fd4981ddb912fd7013c9413311bcd04e12d
phungdai <37787394+phungdai@users.noreply.github.com>
2019-01-17 18:25:56 +0700

    edit index

--- php_mysql/index.php
+++ php_mysql/index.php
@@ -2,20 +2,17 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script type="text/javascript">
-		function xoa(id){
-			var result = window.confirm("Bạn có chắc muốn xoá sản phẩm có mã: "+id);
-			if(result==true){
-				window.location.href= "delete.php?id="+id;
+		function xoa(id) {
+			var result = window.confirm("Bạn có chắc muốn xóa sản phẩm có mã: "+id);
+			if (result==true) {
+				window.location.href="delete.php?id="+id;
 			}
 		}
 	</script>
-	
 </head>
 <body>
-
 <?php
 	//Bước 1
 	require "database.php";
@@ -29,49 +26,49 @@
 	$count = $stmt->rowCount();
 
 	if($count>0){
-		//Biểu diễn dữ liệu
+		//Biểu diễn dữ liện
 		?>
 		<div class="container">
 			<div class="row page-header">
 				<div class="col-xs-6">
-					<h4>Danh sach san pham</h4>
+					<h4>Danh sách sản phẩm</h4>
 				</div>
-				<div class="col-xs-6" style="text-align: right;">
+				<div class="col-xs-6" style="text-align: right">
 					<a href="create.php" class="btn btn-success">Thêm mới</a>
 				</div>
 			</div>
 
-			<table class="table table-hover table-bordered table-responsive">
+			
+			<table class="table table-responsive table-bordered table-hover">
 				<tr>
-					<th>Ten</th><th>Gia</th><th>So luong nhap kho</th><th>Chuc nang</th>
+					<th>Tên</th>
+					<th>Giá</th>
+					<th>Số lượng nhập kho</th>
+					<th>Chức năng</th>
+
 				</tr>
+
 		<?php
-		while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-			echo "<tr>";
-				echo "<td><a href='detail.php?id=".$row['ProductID']."'>";
-				echo $row['ProductName'];
-				echo "</a></td>";
-				echo "<td>".$row['UnitPrice']."</td>";
-				echo "<td>".$row['QuantityPerUnit']."</td>";
-				echo "<td>";
-					$id = $row['ProductID'];
-					echo "<a href='update.php?id=".$row['ProductID']."'>Sua</a>";
-					echo "<a href='#' onclick='xoa(".$id.");'>Xoa</a>";
-				echo "</td>";
-			echo "</tr>";
-		}
+			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+				echo "<tr>";
+					echo "<td><a href='detail.php?id=".$row['ProductID']."'>";
+					echo $row['ProductName'];
+					echo "</a></td>";
+					
+					echo "<td>".$row['UnitPrice']."</td>";
+					echo "<td>".$row['QuantityPerUnit']."</td>";
+					echo "<td>";
+						$id = $row['ProductID'];
+						echo "<a class='btn btn-success' href='update.php?id=".$row['ProductID']."'>Sửa</a>";
+						echo "<a class='btn btn-danger' href='#' onclick='xoa(".$id.");'>Xóa</a>";
+					echo "</td>";
+				echo "</tr>";
+			}
 		?>
 			</table>
 		</div>
 		<?php
-	}
+	}	
 ?>
-
 </body>
 </html>
-
-
-
-
-
-
