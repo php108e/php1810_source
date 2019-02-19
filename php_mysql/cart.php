bbeb6859e8cf55eb8107402caaef80a6800903c2
Tho <thopn.hvcsnd@gmail.com>
2019-02-19 19:12:52 +0700

    phishing cart

--- php_mysql/cart.php
+++ php_mysql/cart.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 if(isset($_GET['id'])){
 	$id = $_GET['id'];
 	$quantity = 1;
@@ -25,9 +26,37 @@ if(isset($_GET['id'])){
 		)
 	);
 
-	// echo "<pre>";
-	// print_r($item);
+	//Kiem tra SESSION cua gio hang xem da ton tai san pham muon mua hay chua
+	if(!empty($_SESSION['cart'])){
+		//Neu da co trong SESSION -> Chi viec tang so luong cua san pham do
+		if(in_array($row[0]['Modem'], array_keys($_SESSION['cart']))){
+			//Tien hang duyet tung phan tu trong SESSION de kiem tra theo 'key'
+			foreach ($_SESSION['cart'] as $key => $value) {
+				if($row[0]['Modem'] == $key){
+					$_SESSION['cart'][$key]['quantity'] += 1;
+				}
+			}
+		}else{
+			//Neu san pham can mua chua co trong gio hang -> Gop san pham moi vao SESSION
+			$_SESSION['cart'] = array_merge($_SESSION['cart'],$item);
+		}
+	}else{
+		//Neu SP muon mua chua co trong SESSION -> Them moi phan tu vao SESSION
+		$_SESSION['cart'] = $item;
+	}
+
+	echo "<PRE>";
+	print_r($_SESSION['cart']);
+
 
 }else{
 
 }
+
+
+
+
+
+
+
+
